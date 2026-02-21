96c316ff323207c6053614d052bf5981b71db8e8
stackedbyisha <ishasinha042940@gmail.com>
2026-02-21 21:10:09 +0530

    Removed node_modules and .env from repo

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
 dotenv.config();
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-app.get("/api/auth/signup", (req,res) => {
-    res.send("Signup endpoint");
-})
-
-app.get("/api/auth/login", (req,res) => {
-    res.send("Login endpoint");
-})
-
-app.get("/api/auth/logout", (req,res) => {
-    res.send("Logout endpoint");
-})
 
 app.listen(PORT, () => console.log("Server running on port: " + PORT));
