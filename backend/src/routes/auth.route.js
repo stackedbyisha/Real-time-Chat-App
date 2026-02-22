1be401419456243e681fda5586fa22206fa272a8
stackedbyisha <ishasinha042940@gmail.com>
2026-02-22 19:00:15 +0530

    modify

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -4,14 +4,14 @@ const router = express.Router();
 
 router.get("/signup", (req,res) => {
     res.send("Signup endpoint");
-})
+});
 
 router.get("/login", (req,res) => {
     res.send("Login endpoint");
-})
+});
 
 router.get("/logout", (req,res) => {
     res.send("Logout endpoint");
-})
+});
 
 export default router;
