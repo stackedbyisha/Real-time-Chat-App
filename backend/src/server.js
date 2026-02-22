2e6fd31f7f2a798a601ad23d8582db6c8e9a8f9e
stackedbyisha <ishasinha042940@gmail.com>
2026-02-23 00:57:46 +0530

    feat(auth): implement welcome email on successful signup

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,13 +5,14 @@ import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
+import { ENV } from "./lib/env.js";
 
 dotenv.config();
 
 const app = express();
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 5000;
+const PORT = ENV.PORT || 5000;
 
 app.use(express.json()); // req.body
 
@@ -19,7 +20,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // Make ready for deployment 
-if (process.env.NODE_ENV === "production") {
+if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));  
   app.get("*", (_, res) => {
     res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
