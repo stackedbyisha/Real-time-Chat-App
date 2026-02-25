6c8e8d6bd085110112c42206273211f2acce4125
stackedbyisha <ishasinha042940@gmail.com>
2026-02-25 23:12:09 +0530

    feat: add signup page with routing and form handling

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -5,7 +5,7 @@ export const ENV = {
     MONGO_URI: process.env.MONGO_URI,
     JWT_SECRET: process.env.JWT_SECRET,
     NODE_ENV: process.env.NODE_ENV,
-    CLIENT_URI: process.env.CLIENT_URI,
+    CLIENT_URL: process.env.CLIENT_URL,
     RESEND_API_KEY: process.env.RESEND_API_KEY,
     EMAIL_FROM: process.env.EMAIL_FROM,
     EMAIL_FROM_NAME: process.env.EMAIL_FROM_NAME,
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import path from "path";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -8,12 +9,12 @@ import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 
 const app = express();
-
 const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 5000;
 
 app.use(express.json()); // req.body
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true}));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
