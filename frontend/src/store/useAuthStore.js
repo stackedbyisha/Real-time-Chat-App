a830d16217d6d7f7ab8b4d3f9908f71925646763
Isha <ishasinha042940@gmail.com>
2026-03-16 23:08:07 +0530

    Update BASE_URL for production environment

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
+  import.meta.env.MODE === "development" ? "https://synqora-backend.onrender.com" : "/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
